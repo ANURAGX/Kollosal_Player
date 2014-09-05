fd494a4d73ace5e64c0c839bba65b814bfa3bb96
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-09-06 00:06:15 +0530

    UI prototype for the app now available....

--- src/drawnzer/anurag/shareplayer/StreamSharePlayer.java
+++ src/drawnzer/anurag/shareplayer/StreamSharePlayer.java
@@ -19,15 +19,140 @@
 
 package drawnzer.anurag.shareplayer;
 
-import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
+import android.view.View;
+import com.astuetz.PagerSlidingTabStrip;
 
-public class StreamSharePlayer extends Activity{
 
+
+public class StreamSharePlayer extends FragmentActivity{
+	
+	private final Handler handler = new Handler();
+	Drawable oldBackground;
+	ViewPager pager;
+	PagerSlidingTabStrip pagerSlideTab;
+	private int currentColor = 0xFF666666;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.share_player);
+		setContentView(R.layout.share_player);		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);		
+		pager = (ViewPager)findViewById(R.id.pager);
+		pagerSlideTab = (PagerSlidingTabStrip)findViewById(R.id.tabs);
+		pager.setAdapter( new MyPagerAdapter(getSupportFragmentManager()));
+		final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+				.getDisplayMetrics());
+		pager.setPageMargin(pageMargin);
+		pagerSlideTab.setViewPager(pager);
+		changeColor(currentColor);
+	}	
+	
+	public void onColorClicked(View v) {
+		int color = Color.parseColor(v.getTag().toString());
+		changeColor(color);
+
+	}
+	private void changeColor(int newColor) {
+
+		pagerSlideTab.setIndicatorColor(newColor);
+
+		// change ActionBar color just if an ActionBar is available
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+			Drawable colorDrawable = new ColorDrawable(newColor);
+			Drawable bottomDrawable = getResources().getDrawable(R.drawable.actionbar_bottom);
+			LayerDrawable ld = new LayerDrawable(new Drawable[] { colorDrawable, bottomDrawable });
+
+			if (oldBackground == null) {
+
+				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+					ld.setCallback(drawableCallback);
+				} else {
+					getActionBar().setBackgroundDrawable(ld);
+				}
+
+			} else {
+
+				TransitionDrawable td = new TransitionDrawable(new Drawable[] { oldBackground, ld });
+
+				// workaround for broken ActionBarContainer drawable handling on
+				// pre-API 17 builds
+				// https://github.com/android/platform_frameworks_base/commit/a7cc06d82e45918c37429a59b14545c6a57db4e4
+				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+					td.setCallback(drawableCallback);
+				} else {
+					getActionBar().setBackgroundDrawable(td);
+				}
+
+				td.startTransition(200);
+
+			}
+
+			oldBackground = ld;
+
+			// http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
+			getActionBar().setDisplayShowTitleEnabled(false);
+			getActionBar().setDisplayShowTitleEnabled(true);
+
+		}
+		currentColor = newColor;
+
+	}
+	
+	private Drawable.Callback drawableCallback = new Drawable.Callback() {
+		@Override
+		public void invalidateDrawable(Drawable who) {
+			getActionBar().setBackgroundDrawable(who);
+		}
+
+		@Override
+		public void scheduleDrawable(Drawable who, Runnable what, long when) {
+			handler.postAtTime(what, when);
+		}
+
+		@Override
+		public void unscheduleDrawable(Drawable who, Runnable what) {
+			handler.removeCallbacks(what);
+		}
+	};
+
+	public class MyPagerAdapter extends FragmentPagerAdapter {
+
+		private final String[] TITLES = { "Categories", "Home", "Top Paid", "Top Free", "Top Grossing", "Top New Paid",
+				"Top New Free", "Trending" };
+
+		public MyPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return TITLES[position];
+		}
+
+		@Override
+		public int getCount() {
+			return TITLES.length;
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return SuperAwesomeCardFragment.newInstance(position);
+		}
 	}	
 }
