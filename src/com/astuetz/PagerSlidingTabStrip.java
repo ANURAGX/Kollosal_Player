c12a4bbad798289a6c66ed1b8d9df50c4d4f5e2e
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-09-21 01:21:38 +0530

    old package removed...

--- src/com/astuetz/PagerSlidingTabStrip.java
+++ src/com/astuetz/PagerSlidingTabStrip.java
@@ -40,7 +40,8 @@ import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import drawnzer.anurag.shareplayer.R;
+import drawnzer.anurag.kollosal.R;
+
 
 
 @SuppressLint("NewApi")
