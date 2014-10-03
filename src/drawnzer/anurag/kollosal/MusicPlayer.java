a4ce9c62af818124d969871a66dcfbb986c51ef8
anurag-dev1512 <anurag.dev1512@gmail.com>
2014-10-03 20:22:20 +0530

    sync commit before starting music service class....

--- src/drawnzer/anurag/kollosal/MusicPlayer.java
+++ src/drawnzer/anurag/kollosal/MusicPlayer.java
@@ -36,15 +36,17 @@ import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * 
  * @author Anurag....
  *
  */
-public class MusicPlayer extends FragmentActivity{
+public class MusicPlayer extends FragmentActivity implements View.OnClickListener{
 
 	private final Handler handler = new Handler();
 	private Drawable oldBackground;
@@ -93,9 +95,6 @@ public class MusicPlayer extends FragmentActivity{
 
 			} else {
 				TransitionDrawable td = new TransitionDrawable(new Drawable[] { oldBackground, ld });
-				// workaround for broken ActionBarContainer drawable handling on
-				// pre-API 17 builds
-				// https://github.com/android/platform_frameworks_base/commit/a7cc06d82e45918c37429a59b14545c6a57db4e4
 				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
 					td.setCallback(drawableCallback);
 				} else {
@@ -105,13 +104,10 @@ public class MusicPlayer extends FragmentActivity{
 				td.startTransition(200);
 			}
 			oldBackground = ld;
-			// http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
 			getActionBar().setDisplayShowTitleEnabled(false);
 			getActionBar().setDisplayShowTitleEnabled(true);
 
 		}
-		//currentColor = newColor;
-
 	}
 	
 	private Drawable.Callback drawableCallback = new Drawable.Callback() {
@@ -151,5 +147,12 @@ public class MusicPlayer extends FragmentActivity{
 			
 		}
 	}
-	
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		
+		}
+	}	
 }
