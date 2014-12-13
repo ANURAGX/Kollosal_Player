6b56bfc7178f1c71d2c6be84c0366531ce8a39a0
Drawnzer <anurag.dev1512@gmail.com>
2014-12-13 16:55:31 +0530

    layout color change.....

--- src/drawnzer/anurag/kollosal/fragments/NetworkStreamFragments.java
+++ src/drawnzer/anurag/kollosal/fragments/NetworkStreamFragments.java
@@ -20,17 +20,20 @@
 
 package drawnzer.anurag.kollosal.fragments;
 
+import drawnzer.anurag.kollosal.R;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.FrameLayout.LayoutParams;
 
+
+/**
+ * 
+ * @author Anurag....
+ *
+ */
 public class NetworkStreamFragments extends Fragment{
 
 	@Override
@@ -42,19 +45,6 @@ public class NetworkStreamFragments extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		
-		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		FrameLayout layout = new FrameLayout(getActivity());
-		layout.setLayoutParams(params);
-	
-		int margin = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics());
-		
-		EditText enterURL = new EditText(getActivity());
-		params.setMargins(margin, margin, margin, margin);
-		enterURL.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-		enterURL.setGravity(Gravity.TOP);
-		enterURL.setHint("Enter URL");
-		layout.addView(enterURL);
-		return layout;
+		return inflater.inflate(R.layout.network_stream, container , false );		
 	}
 }
